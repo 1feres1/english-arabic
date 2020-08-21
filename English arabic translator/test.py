8b97dc3bcfc1029db0a3e7fde79c8b6a8e0554a2
1feres1 <63355746+1feres1@users.noreply.github.com>
2020-08-21 18:57:19 +0100

    Update test.py

---
+++ English arabic translator/test.py
@@ -0,0 +1,8 @@
+from train import * 
+for seq_index in range(100):
+    
+    input_seq = encoder_input_data[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+    print('Decoded sentence:', decoded_sentence)
